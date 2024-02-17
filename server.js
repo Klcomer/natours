43155e4ecb8da05ba52285df16135c01426bdd87
Klcomer <68164495+Klcomer@users.noreply.github.com>
2024-02-17 14:27:33 +0100

    ADD unhandledRejection and uncaughtException errors

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION 🔥🔥🔥 Shutting down...')
+    console.log(err.name, err.message)
+    process.exit(1)
+})
+
 dotenv.config({ path: './config.env' })
 const app = require('./app')
 
@@ -20,15 +27,21 @@ mongoose.connect(DB, {
     poolSize: 10
 }).then((con) => {
     console.log('DB connected succesfully')
-}).catch((err) => {
-    console.error('DB connection error:', err);
-});
+})
 
 
 
 
 
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}`)
+})
+
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 🔥🔥🔥 Shutting down...')
+    console.log(err.name, err.message)
+    server.close(() => {
+        process.exit(1)
+    })
 })
